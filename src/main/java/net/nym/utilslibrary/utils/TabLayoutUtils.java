57ec828e39a5d94b494b09d6a815a6355a3cba46
niyueming <niyueming@163.com>
2017-09-26 20:28:47 +0800

    规范

---
+++ src/main/java/net/nym/utilslibrary/utils/TabLayoutUtils.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2017  Ni YueMing<niyueming@163.com>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+package net.nym.utilslibrary.utils;
+
+import android.content.res.Resources;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.TabLayout;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import java.lang.reflect.Field;
+
+/**
+ * @author niyueming
+ * @date 2017-09-14
+ * @time 15:52
+ */
+
+public class TabLayoutUtils {
+
+    public static void setIndicator(TabLayout tabs, int leftDip, int rightDip) {
+        Class<?> tabLayout = tabs.getClass();
+        Field tabStrip = null;
+        try {
+            tabStrip = tabLayout.getDeclaredField("mTabStrip");
+            tabStrip.setAccessible(true);
+            LinearLayout llTab = (LinearLayout) tabStrip.get(tabs);
+
+            int left = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, leftDip, Resources.getSystem().getDisplayMetrics());
+            int right = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, rightDip, Resources.getSystem().getDisplayMetrics());
+
+            for (int i = 0; i < llTab.getChildCount(); i++) {
+                View child = llTab.getChildAt(i);
+                child.setPadding(0, 0, 0, 0);
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1);
+                params.leftMargin = left;
+                params.rightMargin = right;
+                child.setLayoutParams(params);
+                child.invalidate();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
