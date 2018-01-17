686c911b8cff8952f54f6e30f0b62a61506ba2fb
niyueming <niyueming@163.com>
2018-01-17 15:14:46 +0800

    ⚡️ - 优化

--- src/main/java/net/nym/utilslibrary/utils/PickerUtils.java
+++ src/main/java/net/nym/utilslibrary/utils/PickerUtils.java
@@ -18,6 +18,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.NumberPicker;
 import android.widget.TimePicker;
@@ -159,5 +160,51 @@ public class PickerUtils {
             }
         }
         return npList;
+    }
+
+    /**
+     * 调整FrameLayout大小
+     * @param tp
+     */
+    public static void resizePikcer(FrameLayout tp){
+        List<NumberPicker> npList = findNumberPicker2(tp);
+        for(NumberPicker np:npList){
+            resizeNumberPicker(np);
+        }
+    }
+
+    /**
+     * 得到viewGroup里面的numberpicker组件
+     * @param viewGroup
+     * @return
+     */
+    private static List<NumberPicker> findNumberPicker2(ViewGroup viewGroup){
+        List<NumberPicker> npList = new ArrayList<NumberPicker>();
+        View child = null;
+        if(null != viewGroup){
+            for(int i = 0;i<viewGroup.getChildCount();i++){
+                child = viewGroup.getChildAt(i);
+                if(child instanceof NumberPicker){
+                    npList.add((NumberPicker)child);
+                }
+                else if(child instanceof LinearLayout){
+                    List<NumberPicker> result = findNumberPicker((ViewGroup)child);
+                    if(result.size()>0){
+                        return result;
+                    }
+                }
+            }
+        }
+        return npList;
+    }
+
+    /*
+		 * 调整numberpicker大小
+		 */
+    private static void resizeNumberPicker(NumberPicker np){
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ContextUtils.dip2px(np.getContext(),48)
+                , LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(ContextUtils.dip2px(np.getContext(),3), 0, ContextUtils.dip2px(np.getContext(),3), 0);
+        np.setLayoutParams(params);
     }
 }
